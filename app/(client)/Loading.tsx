bab22f7b351c10873ceaebc05b646cff91c4f740
sanjay-k-m <sanjay_k.m@outlook.com>
2025-03-17 23:12:12 +0530

    cart page completed
    used zustand to manage states

--- app/(client)/Loading.tsx
+++ app/(client)/Loading.tsx
@@ -1,26 +1,11 @@
-"use client";
-import { motion } from "motion/react";
-import { Loader2 } from "lucide-react";
-import Logo from "@/components/Logo";
 
-const Loading = () => {
+import Loading from "@/components/Loading";
+const loading = () => {
   return (
-    <div className=" min-h-screen w-full bg-white left-0 top-0 flex items-center justify-center ">
-      <div className="flex flex-col justify-center items-center gap-1">
-        <Logo>Zylera</Logo>
-        <motion.div
-          animate={{ scale: [1, 1.1, 1] }}
-          transition={{ repeat: Infinity, duration: 1.5 }}
-          className="flex items-center space-x-2 text-green-800"
-        >
-          <Loader2 className=" animate-spin" />
-          <span className="font-semibold tracking-wide">
-            zylera is loading...
-          </span>
-        </motion.div>
-      </div>
-    </div>
+   <div>
+    <Loading />
+   </div>
   );
 };
 
-export default Loading;
+export default loading;
